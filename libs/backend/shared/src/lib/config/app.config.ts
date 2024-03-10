e80df23e75e4001d884a0a55850ae9812c0356d1
Ceyed <saeed.sarmad.976@gmail.com>
2024-03-10 15:54:59 +0330

    feat: add .env to gitignore

--- libs/backend/shared/src/lib/config/app.config.ts
+++ libs/backend/shared/src/lib/config/app.config.ts
@@ -31,10 +31,9 @@ export class AppConfig {
 }
 
 export const appConfig = registerConfig(AppConfig, () => {
-  const port: string = process.env[APP_CONFIG.PORT];
   return new AppConfig({
     host: process.env[APP_CONFIG.HOST],
-    port: port ? +port : undefined,
+    port: process.env[APP_CONFIG.PORT] ? +process.env[APP_CONFIG.PORT] : undefined,
     clientHost: process.env[APP_CONFIG.CLIENT_HOST],
     nodeEnv: process.env[APP_CONFIG.NODE_ENV] || 'development',
   });
